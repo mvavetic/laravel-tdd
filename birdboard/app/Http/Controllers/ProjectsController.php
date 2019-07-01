e16a74f5b7d8774c6be91462efdfc9264c9785e3
matijavavetic <47482084+matijavavetic@users.noreply.github.com>
2019-07-01 19:43:29 +0200

    Notes and policy

--- birdboard/app/Http/Controllers/ProjectsController.php
+++ birdboard/app/Http/Controllers/ProjectsController.php
@@ -1,25 +1,79 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Project;
 
-use Illuminate\Http\Request;
+use App\Project;
 
 class ProjectsController extends Controller
 {
+    /**
+     * View all projects.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        $projects = Project::all();
+        $projects = auth()->user()->projects;
 
         return view('projects.index', compact('projects'));
     }
 
+    /**
+     * Show a single project.
+     *
+     * @param Project $project
+     *
+     * @return \Illuminate\Http\Response
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function show(Project $project)
+    {
+        $this->authorize('update', $project);
+
+        return view('projects.show', compact('project'));
+    }
+
+    /**
+     * Create a new project.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('projects.create');
+    }
+
+    /**
+     * Persist a new project.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function store()
     {
-        $attributes = request()->validate(['title' => 'required', 'description' => 'required']);
+        $attributes = request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'notes' => 'min:3'
+        ]);
 
-        Project::create($attributes);
+        $project = auth()->user()->projects()->create($attributes);
 
-        return redirect('/projects');
+        return redirect($project->path());
+    }
+
+    /**
+     * Update the project.
+     *
+     * @param  Project $project
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function update(Project $project)
+    {
+        $this->authorize('update', $project);
+
+        $project->update(request(['notes']));
+
+        return redirect($project->path());
     }
 }
--- birdboard/database/factories/ProjectFactory.php
+++ birdboard/database/factories/ProjectFactory.php
@@ -1,13 +1,11 @@
 <?php
 
-/* @var $factory \Illuminate\Database\Eloquent\Factory */
-
-use App\Project;
 use Faker\Generator as Faker;
 
-$factory->define(Project::class, function (Faker $faker) {
+$factory->define(App\Project::class, function (Faker $faker) {
     return [
-        'title' => $faker->sentence,
-        'description' => $faker->paragraph
+        'title' => $faker->sentence(4),
+        'description' => $faker->sentence(4),
+        'owner_id' => factory(App\User::class)
     ];
 });
